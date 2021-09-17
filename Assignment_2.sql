2fb91a5c799e2ea6f7c560449a5b377a57900c16
Nitesh Kumar <niteshkumar8833@gmail.com>
2021-09-17 21:36:19 +0530

    Add files via upload

---
+++ Assignment_2.sql
@@ -0,0 +1,11 @@
+1)
+SELECT LEN(FIRST_NAME)||SUBSTRING( FIRST_NAME, 1, 3 ) FROM WORKER;
+
+2)
+
+SELECT INSTR(first_name, "A") FROM worker WHERE first_name = "Amitabh";
+
+3)
+
+select FIRST_NAME,MAX(Salary),department from worker
+
