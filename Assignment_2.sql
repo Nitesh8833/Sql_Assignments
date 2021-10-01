85c1f394433aa37fd578d50039f696b35569b691
Nitesh Kumar <niteshkumar8833@gmail.com>
2021-10-01 18:09:02 +0530

    Add files via upload

--- Assignment_2.sql
+++ Assignment_2.sql
@@ -5,7 +5,8 @@ SELECT SUBSTRING( FIRST_NAME, 1, 3 ) FROM WORKER;
 
 SELECT CHARINDEX('a',FIRST_NAME) from worker where FIRST_NAME = 'Amitabh';
 
-3)SELECT department, MAX(Salary) as highest_salary FROM worker GROUP BY department;
-
-******select FIRST_NAME,MAX(Salary),distinct(department) from worker*****
+3)select w.first_name, w.department,w.salary from worker w  
+  join (select max(salary) as maxsal, department as dept  
+          from worker group by department) d  
+    on d.dept = w.department and w.salary = d.maxsal; 
 
